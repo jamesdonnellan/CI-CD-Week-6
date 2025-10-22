e83ae66ee8715c86e40b6f9a7d05d95b3d5f0c96
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-10-22 14:41:00 +0100

    Added controller handling to intercept validation errors and return custom error responses

--- src/main/java/ie/atu/week5/cicdweek5/errorHandling/GlobalExceptionHandling.java
+++ src/main/java/ie/atu/week5/cicdweek5/errorHandling/GlobalExceptionHandling.java
@@ -1,4 +1,29 @@
 package ie.atu.week5.cicdweek5.errorHandling;
 
-public class GlobalExceptionHandling {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandling
+{
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ExceptionDetails>> showErrorDetails(MethodArgumentNotValidException mae)
+    {
+        List<ExceptionDetails> errorList = new ArrayList<>();
+        for(FieldError fieldError : mae.getBindingResult().getFieldErrors())
+        {
+            ExceptionDetails exceptionDetails = new ExceptionDetails();
+            exceptionDetails.setFieldName(fieldError.getField());
+            exceptionDetails.setFieldValue(fieldError.getDefaultMessage());
+            errorList.add(exceptionDetails);
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorList);
+    }
 }
